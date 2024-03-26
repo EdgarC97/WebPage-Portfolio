259d64d6bc1d1101f13b8a007e0b74c3201ef0df
EdgarC97 <ecardona742@gmail.com>
2024-03-25 23:46:46 -0500

    Se realizaron modificaciones en todos los HTMLS y CSS , ademas se agrega archivo JS para hamburguer menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", ()=>{
+    const nav = document.getElementById("nav");
+        nav.classList.toggle("active")
+})
+
